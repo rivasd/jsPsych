f8b8e4261a51362c284b541819822f0c916cb6c9
rivasdaniel <danielrivas1992@gmail.com>
2016-03-02 17:35:21 -0500

    html-plugin: check_fn can now return dict to merge with collected data

--- plugins/jspsych-html.js
+++ plugins/jspsych-html.js
@@ -20,7 +20,21 @@ jsPsych.plugins.html = (function() {
     // this evaluates the function and replaces
     // it with the output of the function
     trial = jsPsych.pluginAPI.evaluateFunctionParameters(trial, ["check_fn"]);
-
+    
+    //I also propose an optional default function that scans all input or textarea elements inside the target element, and builds a key value pair
+    function findAllInput(elt){
+    	var dict={};
+    	var inputElts = elt.find(":input"); //get all input elements within the target element
+    	inputElts.each(function(idx, input){
+    		var value = $.trim(input.val()); // do not add to value dict if value is empty string
+    		if(value != ''){
+    			dict[input.attr("name")] = input.val();
+    		}
+    	});
+    	return dict;
+    }
+    
+    
     var url = trial.url;
     if (trial.force_refresh) {
       url = trial.url + "?time=" + (new Date().getTime());
@@ -29,13 +43,29 @@ jsPsych.plugins.html = (function() {
     display_element.load(trial.url, function() {
       var t0 = (new Date()).getTime();
       var finish = function() {
-        if (trial.check_fn && !trial.check_fn(display_element)) return;
+    	// going to make some changes to allow the check_fn function to collect data from the html element
+    	// it will be expected to return a dictionnary that will be merged to what is already provided as data
+    	var extraData = {};
+    	  
+        if (trial.check_fn){
+        	
+        	extraData = trial.check_fn(display_element, findAllInput(display_element)); // new parameter: dictionnay made of the name og all input elements and their value, if non empty
+        	if(extraData == false || $.isEmptyObject(extraData)){ //if it returns false, simply keep old behavior, I hope strict comparison is enough
+        		return;											// I also reject empty dictionnaries
+        	}
+        }
         if (trial.cont_key) $(document).unbind('keydown', key_listener);
         var trial_data = {
           rt: (new Date()).getTime() - t0,
           url: trial.url
         };
+        
+        if (typeof extraData != 'boolean'){
+        	trial_data = $.extend({}, trial_data, extraData); // merge the data. Shallow merge on purpose
+        }
+        
         display_element.empty();
+        
         jsPsych.finishTrial(trial_data);
       };
       if (trial.cont_btn) $('#' + trial.cont_btn).click(finish);
