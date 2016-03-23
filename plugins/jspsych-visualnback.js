fb38c26233d65544617cd099996f99a483f60e6c
catou239 <catou239@hotmail.com>
2016-03-23 17:54:05 -0400

    Preparation of the fuction init in the nback task that serve to place targets on the task sreen

--- plugins/jspsych-visualnback.js
+++ plugins/jspsych-visualnback.js
@@ -1,6 +1,29 @@
 jsPsych.plugins['visualnback'] = (function(){
+	/**Première fois que le trial roule placer les target(ronds) 9? sur la page 
+	 * -Pour chaque trial
+	 *       Changer la couleur d'une image
+	 *       
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * **/
+	
 
   var plugin = {};
+  plugin.targets = [];
+  
+  plugin.init = function(targetcount){
+	  var height = jsPsych.getDisplayElement().heigth();
+	  var width = jsPsych.getDisplayElement().width();
+	  var smallest = Math.min(height,width);
+	  var size = smallest/(targetcount+2);
+	  
+	  for(var i = 0; i< targetcount; i++){
+		  	var $target = $("<div></div>");
+	  }
+  }
 
   plugin.trial = function(display_element, trial){
     jsPsych.finishTrial();
