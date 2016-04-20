d15037df2db648904ddd6c3e06cf3afeaf52817c
catou239 <catou239@hotmail.com>
2016-04-20 15:37:12 -0400

    fonction init finie

--- plugins/jspsych-visualnback.js
+++ plugins/jspsych-visualnback.js
@@ -7,7 +7,7 @@ jsPsych.plugins['visualnback'] = (function(){
   
   function getRandomArbitrary(min, max){
 	  
-	  return Math.random()*(max-min)+min;
+	  return Math.floor(Math.random()*(max-min)+min);
   }
   
   plugin.init = function(targetcount){
@@ -15,54 +15,69 @@ jsPsych.plugins['visualnback'] = (function(){
 	  var width = jsPsych.getDisplayElement().width();
 	  var smallest = Math.min(height,width);
 	  var size = smallest/(targetcount+2);
-	  allTargets = [];	
+	  var allTargets = [];	
 	  
 	  for(var i = 0; i < targetcount; i++){
-		  	var $target = allTargets[i] = $("<div></div>");
+		  	var $target = $("<div></div>");
 		  	$target.css("position","relative");
-		  	  	
-		  	var X = $target.css("left",getRandomArbitrary(size,width-size));
-		  	var Y = $target.css("top",getRandomArbitrary(size,height-size));
+		  	
+		  	var point = {
+		  		node: $target,
+		  		X: 0,
+		  		Y: 0		
+		  	}
+		  	
+		  	allTargets[i]=point;	
+		  	point.X = getRandomArbitrary(size,width-size);
+		  	point.Y = getRandomArbitrary(size,height-size);
 		  	
 		  	if(i > 0){	
 		  		for(var j = 0; j < i ; j){
-		  			if ((Math.abs(allTargets[j].css("left") - allTargets[i].css("left")) > size/2) && 
-		  					(Math.abs(allTargets[j].css("top") - allTargets[i].css("top")) > size/2)){
-		  					if(X < (width - 2*size)){
-		  						X = X + size/2;
+		  			if ((Math.abs(allTargets[j].X - allTargets[i].Y) > size/2) && 
+		  					(Math.abs(allTargets[j].Y - allTargets[i].Y) > size/2)){
+		  					if(point.X < (width - 2*size)){
+		  						point.X += size/2;
 		  					}	
-		  					else X = size;
+		  					else point.X = size;
 		  					j=0;
 		  				}
-		  			j++
-		  			
+		  			j++	  			
 		  		}
-		  	} 
-	    	
+		  	}     	
 	  }
-  /* 1. attribuer une couleur aux target
-   * 2. Pour n+1 fois
-   * 	2.1 changer la couleur d'un target (aléatoirement?)
-   * 	2.2 mettre le no. du target qui a changé de couleur dans un tableau
-   * 3. Pour le nombre de fois désirées du trial
-   * 	3.1 changer la couleur
-   * 	3.2 mettre le no. du target qui a changé de couleur dans un tableau
-   * 	3.3 demander à l'utilisateur le target qui a changé de couleur n fois avant
-   * 	3.4 mettre la réponse dans un tableau
-   * 	3.5 Comparer les tableaux
-   * 		3.5.1 Si la réponse est pareille que le n-back
-   * 			3.5.1.1 Afficher une réussite 
-   * 			3.5.1.2 Afficher un échec
-   * 	3.6 Enregistrer les données 
-   * */
-
+	  
+	  allTargets.forEach(function(point){
+		  jsPsych.getDisplayElement().append(point.node);
+		  point.node.css("position","relative");
+		  point.node.css("left",point.X);
+		  point.node.css("top", point.Y);	  
+	  });
+	  
+	  plugin.targets = allTargets;
 	  
   }
 
   plugin.trial = function(display_element, trial){
+	  
+	  
     jsPsych.finishTrial(data);
   }
 
   return plugin;
 
 })();
+/**1. attribuer une couleur aux target
+ * 2. Pour n+1 fois
+ * 	2.1 changer la couleur d'un target (aléatoirement?)
+ * 	2.2 mettre le no. du target qui a changé de couleur dans un tableau
+ * 3. Pour le nombre de fois désirées du trial
+ * 	3.1 changer la couleur
+ * 	3.2 mettre le no. du target qui a changé de couleur dans un tableau
+ * 	3.3 demander à l'utilisateur le target qui a changé de couleur n fois avant
+ * 	3.4 mettre la réponse dans un tableau
+ * 	3.5 Comparer les tableaux
+ * 		3.5.1 Si la réponse est pareille que le n-back
+ * 			3.5.1.1 Afficher une réussite 
+ * 			3.5.1.2 Afficher un échec
+ * 	3.6 Enregistrer les données 
+ * **/
