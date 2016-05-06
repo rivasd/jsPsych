15c4116f88c1b01a32c9632fe6ee2a84b3678eb3
rivasdaniel <danielrivas1992@gmail.com>
2016-05-05 23:24:42 -0400

    CardGenerator tested and working!

--- plugins/jspsych-wcst.js
+++ plugins/jspsych-wcst.js
@@ -9,7 +9,7 @@
 jsPsych.plugins['wcst'] = (function(){
 
   var plugin = {};
-  
+  var initialized = false;
   /**@type {string} represents which dimension serves currently as the rule*/
   plugin.rule= undefined;
   
@@ -19,21 +19,205 @@ jsPsych.plugins['wcst'] = (function(){
   /**@type {string} holds the value of the last rule, useful to identify perseveration errors*/
   plugin.lastRule = undefined;
   
-  function ImageGenerator(canvas){
+  
+  
+  /**
+   * A module to generate the card stimuli on-the-fly using a hidden HTML5 canvas element. 
+   * Eliminates the need for researchers to find, create, and store images on their server and prevents having to specify and handle naming conventions
+   * just to find and present the right card (e.g. forcing to name cards like <number>_<color>_<shape>.png or what not).
+   * Multiple options for card creation can be specified, allowing expansion of experimental designs and maybe reuse for further stimuli generation tasks.
+   * 
+   * 
+   */
+  function CardGenerator(opts){
 	  var module = {};
 	  
+	  var height = opts.height;
+	  var width = opts.width;
+	  var size = Math.floor(width/5);
+	  var backgroundColor = opts.backgroundColor;
+	  
+	  var context;
+	  var vault={};
+	  var numberOfCards=0;
+	  var keyPoints={};
+	  
+	  var numbers=['1', '2', '3', '4'];
+	  var shapes=['triangle', 'square', 'circle', 'cross'];
+	  var colors=['red', 'yellow', 'blue', 'green'];
+	  var ready=false;
+	  
+	  /**
+	   * Initializes the data structure that will hold our card images in a nicely indexed way.
+	   */
+	  function setupVault(){
+		  numbers.forEach(function(number, i, array) {
+			  vault[number]= {};
+		  	shapes.forEach(function(shape, j, array) {
+		  		vault[number][shape] = {};
+		  		colors.forEach(function(color, k, array) {
+		  			var cardImg = new Image();
+		  			vault[number][shape][color] = cardImg;
+		  			numberOfCards++;
+		  		});
+		  	});
+		  });
+	  }
+	  
+	  /**
+	   * Specifies in advance all the positions on the card (equivalently, the canvas) where we might want to draw a centered figure.
+	   * Computes all the positions used when drawing one to four figures on a card and stores them in the keyPoints private object.
+	   */
+	  function findKeyPoints(){
+		  //we defined that exactly 5 shape-widths should fit along the width of a card, allowing for 1 width's worth of clearance on both sides + 3 possible horizontal positions
+		  var shapeWidth = Math.floor(width/5);
+		  //the min distance a shape's center should be from the card edge
+		  var clearance = Math.ceil(shapeWidth*1.5);
+		  var acrossX = 2*shapeWidth;
+		  
+		  keyPoints.center = [Math.floor(width/2), Math.floor(height/2)];
+		  keyPoints.topLeft = [Math.floor(clearance), Math.floor(clearance)];
+		  keyPoints.topRight = [Math.floor(width-clearance), Math.floor(clearance)];
+		  keyPoints.lowLeft = [Math.floor(clearance), Math.floor(height-clearance)];
+		  keyPoints.lowRight = [Math.floor(width-clearance), Math.floor(height-clearance)];
+		  keyPoints.centerRight = [Math.floor(width-clearance), Math.floor(height/2)];
+	  }
+	  
+	  
+	  function drawSquare(pos, color){
+		  context.fillStyle = color;
+		  context.fillRect(pos[0]-(size/2), pos[1]-(size/2), size, size);
+	  }
+	  
+	  function drawCircle(pos, color){
+		  context.fillStyle = color;
+		  context.beginPath();
+		  context.arc(pos[0], pos[1], size/2, 0, Math.PI*2, false);
+		  context.fill();
+	  }
+	  
+	  function drawCross(pos, color){
+		  
+		  var slimFactor = 0.3; //one of my best variable names yet :)
+		  var smallSide = Math.floor(slimFactor * size);
+		  var verticalRectOrigin = [pos[0]-(smallSide/2), pos[1]-(size/2)];
+		  var horizontalRectOrigin = [pos[0]-(size/2), pos[1]-(smallSide/2)];
+		  
+		  context.fillStyle = color;
+		  context.fillRect(verticalRectOrigin[0], verticalRectOrigin[1], smallSide, size);
+		  context.fillRect(horizontalRectOrigin[0], horizontalRectOrigin[1], size, smallSide);
+	  }
+	  
+	  function drawTriangle(pos, color){
+		  //never thought I'd have to brush up my trig for this...
+		  var smallGap = Math.floor(size*(Math.tan(Math.PI/6)/2));
+		  var bigGap = Math.floor((size/2) / Math.cos(Math.PI/6));
+		  
+		  context.fillStyle = color;
+		  context.beginPath();
+		  context.moveTo(pos[0]+(size/2), pos[1]+smallGap);
+		  context.lineTo(pos[0]-(size/2), pos[1]+smallGap);
+		  context.lineTo(pos[0], pos[1]-bigGap);
+		  context.fill();
+	  }
+	  
+	  function drawShapeAt(shape, pos, color){
+		  var drawFunc;
+		  if(shape === 'square'){
+			  drawFunc = drawSquare;
+		  }
+		  else if(shape === 'circle'){
+			  drawFunc = drawCircle;
+		  }
+		  else if(shape === 'cross'){
+			  drawFunc = drawCross;
+		  }
+		  else if(shape ==='triangle'){
+			  drawFunc = drawTriangle;
+		  }
+		  
+		  drawFunc(pos, color);
+	  }
+	  
+	  function getPointSet(qtt){
+		  var points;
+		  switch (qtt){
+		  case 1:
+			  points=[keyPoints.center];
+			  break;
+		  case 2:
+			  points=[keyPoints.topLeft, keyPoints.lowRight];
+			  break;
+		  case 3:
+			  points=[keyPoints.topLeft, keyPoints.lowLeft, keyPoints.centerRight];
+			  break;
+		  case 4:
+			  points=[keyPoints.topLeft, keyPoints.topRight, keyPoints.lowLeft, keyPoints.lowRight];
+			  break;
+		  default:
+			  throw "We do not support more than 4 figures in the card";
+			  break;
+		  	
+		  }
+		  return points;
+	  }
+	  
+	  /**
+	   * Main function that actually executes the drawing
+	   */
+	  function generate(shape, color, number){
+		  var positions = getPointSet(number); //get a list of all the coords. where we will need to draw an image
+		  context.clearRect(0, 0, width, height); //clear the canvas before starting
+		  
+		  context.fillStyle = backgroundColor; // drawing the background of the card
+		  context.fillRect(0, 0, width, height); 
+		  
+		  positions.forEach(function(position, i, array) {
+		  	drawShapeAt(shape, position, color); //draw the shapes
+		  });
+		  var card = canvas[0].toDataURL();
+		  return card;
+	  }
+	  
+	  /**
+	   * Sets up the vault with empty images, creates and appends the canvas element that will be used to draw the cards.
+	   */
 	  module.init = function(){
-		  
+		  setupVault();
+		  findKeyPoints();
+		  canvas = $('<canvas height="'+height+'" width="'+width+'"></canvas>', {
+			 class: 'jspsych-wcst-canvas',
+			 id:'wcst-canvas',
+			 display:'none'
+		  });
+		  opts.target.append(canvas);
+		  context = canvas[0].getContext("2d");
 	  }
 	  
-	  module.get = function(number, colour, shape){
+	  /**
+	   * Creates the whole set of cards as img elements 
+	   */
+	  module.makeDeck = function(){
 		  
+		  numbers.forEach(function(number, i, array) {
+		  	shapes.forEach(function(shape, j, array) {
+		  		colors.forEach(function(color, k, array) {
+		  			vault[number][shape][color].src = generate(shape, color, parseInt(number));
+		  		});
+		  	});
+		  });
+		  ready=true;
 	  }
 	  
+	  module.get = function(number, shape, color){
+		  return vault[number][shape][color];
+	  }
+	  
+	  module.init();
 	  return module;
   }
   
-  plugin.init = function(){
+  plugin.init = function(opts){
 	  plugin.viewport = $("<table></table>");
 	  plugin.cards = $("<tr></tr>");
 	  plugin.viewport.append(plugin.cards);
@@ -48,16 +232,25 @@ jsPsych.plugins['wcst'] = (function(){
 	  
 	  var canvas = $("<canvas></canvas>");
 	  
-	  jsPsych.getDisplayTarget().append(plugin.viewport);
+	  jsPsych.getDisplayElement().append(plugin.viewport);
 	  
-	  plugin.generator = ImageGenerator();
+	  plugin.generator = CardGenerator({
+		  width: 350,
+		  height: 450,
+		  backgroundColor: 'grey',
+		  target:jsPsych.getDisplayElement()
+	  });
+	  plugin.generator.makeDeck();
+	  
 	  
 	  plugin.choices = []
 	  for(var i=0;i<4;i++){
-		  choices[i] = {}
-		  choices[i].img = new Image()
-		  choices[i].img= 'card-'+i.toString();
+		  plugin.choices[i] = {}
+		  plugin.choices[i].img = new Image()
+		  plugin.choices[i].img= 'card-'+i.toString();
 	  }
+	  
+	  plugin.viewport.append(plugin.generator.get('3', 'cross', 'red'));
 	  
   }
   
@@ -105,7 +298,7 @@ jsPsych.plugins['wcst'] = (function(){
 	   * Updates the consecutive correct answers counter, or sets it to zero if the answer was wrong
 	   */
 	  function keepScore(answer){
-		  
+		  //TODO: don't forget that the first error after a rule change cannot really be counted as a perseveration since there is no prior warning that the rule is now invalid
 	  }
 	  
 	  /**
@@ -116,6 +309,11 @@ jsPsych.plugins['wcst'] = (function(){
 		  plugin.choices[this.id];
 		  //bla blabla
 	  }
+	  
+	  if(!initialized){
+		  plugin.init();
+	  }
+	  
 	  
     jsPsych.finishTrial();
   }
