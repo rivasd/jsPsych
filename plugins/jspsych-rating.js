801fd590e7c10dfd372334c647c3472c64d78532
catou239 <catou239@hotmail.com>
2016-09-27 17:36:47 -0400

    jsPsych-rating finished and successfully tested

--- plugins/jspsych-rating.js
+++ plugins/jspsych-rating.js
@@ -5,6 +5,118 @@ jsPsych.plugins["rating"] = (function() {
   jsPsych.pluginAPI.registerPreload('single-stim', 'stimulus', 'image');
   
 
+  plugin.show_response_slider = function (display_element, trial) {
+
+
+      // create slider
+      display_element.append($('<div>', {
+        "id": 'slider',
+        "class": 'sim'
+      }));
+
+      $("#slider").slider({
+        value: Math.ceil(trial.intervals / 2),
+        min: 1,
+        max: trial.intervals,
+        step: 1,
+      });
+
+      // show tick marks
+      if (trial.show_ticks) {
+        for (var j = 1; j < trial.intervals - 1; j++) {
+          $('#slider').append('<div class="slidertickmark"></div>');
+        }
+
+        $('#slider .slidertickmark').each(function(index) {
+          var left = (index + 1) * (100 / (trial.intervals - 1));
+          $(this).css({
+            'position': 'absolute',
+            'left': left + '%',
+            'width': '1px',
+            'height': '100%',
+            'background-color': '#222222'
+          });
+        });
+      }
+
+      // create labels for slider
+      display_element.append($('<ul>', {
+        "id": "sliderlabels",
+        "class": 'sliderlabels',
+        "css": {
+          "width": "100%",
+          "height": "3em",
+          "margin": "10px 0px 0px 0px",
+          "padding": "0px",
+          "display": "block",
+          "position": "relative"
+        }
+      }));
+
+      for (var j = 0; j < trial.labels.length; j++) {
+        $("#sliderlabels").append('<li>' + trial.labels[j] + '</li>');
+      }
+
+      // position labels to match slider intervals
+      var slider_width = $("#slider").width();
+      var num_items = trial.labels.length;
+      var item_width = slider_width / num_items;
+      var spacing_interval = slider_width / (num_items - 1);
+
+      $("#sliderlabels li").each(function(index) {
+        $(this).css({
+          'display': 'inline-block',
+          'width': item_width + 'px',
+          'margin': '0px',
+          'padding': '0px',
+          'text-align': 'center',
+          'position': 'absolute',
+          'left': (spacing_interval * index) - (item_width / 2)
+        });
+      });
+
+      //  create button
+      display_element.append($('<button>', {
+        'id': 'next',
+        'class': 'sim',
+        'html': 'Submit Answer'
+      }));
+   }
+  
+   plugin.endTrial = function(data, trial, display_element) {
+
+       // kill any remaining setTimeout handler
+       for (var i = 0; i < trial.setTimeoutHandlers.length; i++) {
+    	   clearTimeout(trial.setTimeoutHandlers[i]);
+       }
+   
+	   data.timeout = false; //quick hack for something I need right now
+	   if(data.rating === undefined){
+		   data.rating = null;
+		   data.timeout = true;
+	   }
+	   
+	   
+	   if(trial.return_stim){
+		   data.stimulus = JSON.stringify([trial.stimuli[0], trial.stimuli[1]]);
+	   }
+	   // goto next trial in block
+	   display_element.html('');
+	   
+	   if(data.rt === -1){
+	   	//this was a timeout
+		   	display_element.append(trial.timeout_message);
+		   	trial.setTimeoutHandlers.push(setTimeout(function(){
+		   		display_element.empty();
+		   		jsPsych.finishTrial(data);
+		   	},trial.timeout_message_timing))
+	   }
+	   else{
+		   jsPsych.finishTrial(data);
+	   }
+    };
+  
+
   plugin.trial = function(display_element, trial) {
 
     // if any trial variables are functions
@@ -20,213 +132,53 @@ jsPsych.plugins["rating"] = (function() {
     trial.is_html = (typeof trial.is_html == 'undefined') ? false : trial.is_html;
     trial.prompt = trial.prompt || "";
     
-    trial.labels = (typeof trial.labels === 'undefined') ? ["Not at all similar", "Identical"] : trial.labels;
-    trial.intervals = trial.intervals || 100;
+    trial.labels = (typeof trial.labels === 'undefined') ? ["Hate", "Love"] : trial.labels;
+    trial.intervals = trial.intervals || 7;
     trial.show_ticks = (typeof trial.show_ticks === 'undefined') ? false : trial.show_ticks;
 
     // this array holds handlers from setTimeout calls
     // that need to be cleared if the trial ends early
-    var setTimeoutHandlers = [];
+    trial.setTimeoutHandlers = [];
     
-    // display stimulus
+    //Show the stimulus
     if (!trial.is_html) {
-      display_element.append($('<img>', {
-        src: trial.stimulus,
-        id: 'jspsych-single-stim-stimulus'
-      }));
-    } else {
-      display_element.append($('<div>', {
-        html: trial.stimulus,
-        id: 'jspsych-single-stim-stimulus'
-      }));
-    }
-
-    //show prompt if there is one
-    if (trial.prompt !== "") {
-      display_element.append(trial.prompt);
-    }
-  
-    show_response_slider(display_element, trial);
+    	display_element.append($('<img>', {
+    		src: trial.stimulus,
+	        id: 'jspsych-single-stim-stimulus'
+	    	}));
+	    } 
+    else {
+	      display_element.append($('<div>', {
+	        html: trial.stimulus,
+	        id: 'jspsych-single-stim-stimulus'
+	      }));
+	}
     
-    function show_response_slider(display_element, trial) {
-
-        var startTime = (new Date()).getTime();
-
-        // create slider
-        display_element.append($('<div>', {
-          "id": 'slider',
-          "class": 'sim'
-        }));
-
-        $("#slider").slider({
-          value: Math.ceil(trial.intervals / 2),
-          min: 1,
-          max: trial.intervals,
-          step: 1,
-        });
-
-        // show tick marks
-        if (trial.show_ticks) {
-          for (var j = 1; j < trial.intervals - 1; j++) {
-            $('#slider').append('<div class="slidertickmark"></div>');
-          }
-
-          $('#slider .slidertickmark').each(function(index) {
-            var left = (index + 1) * (100 / (trial.intervals - 1));
-            $(this).css({
-              'position': 'absolute',
-              'left': left + '%',
-              'width': '1px',
-              'height': '100%',
-              'background-color': '#222222'
-            });
-          });
-        }
-
-        // create labels for slider
-        display_element.append($('<ul>', {
-          "id": "sliderlabels",
-          "class": 'sliderlabels',
-          "css": {
-            "width": "100%",
-            "height": "3em",
-            "margin": "10px 0px 0px 0px",
-            "padding": "0px",
-            "display": "block",
-            "position": "relative"
-          }
-        }));
-
-        for (var j = 0; j < trial.labels.length; j++) {
-          $("#sliderlabels").append('<li>' + trial.labels[j] + '</li>');
-        }
-
-        // position labels to match slider intervals
-        var slider_width = $("#slider").width();
-        var num_items = trial.labels.length;
-        var item_width = slider_width / num_items;
-        var spacing_interval = slider_width / (num_items - 1);
-
-        $("#sliderlabels li").each(function(index) {
-          $(this).css({
-            'display': 'inline-block',
-            'width': item_width + 'px',
-            'margin': '0px',
-            'padding': '0px',
-            'text-align': 'center',
-            'position': 'absolute',
-            'left': (spacing_interval * index) - (item_width / 2)
-          });
-        });
-
-        //  create button
-        display_element.append($('<button>', {
-          'id': 'next',
-          'class': 'sim',
-          'html': 'Submit Answer'
-        }));
-
-        // if prompt is set, show prompt
-        if (trial.prompt !== "") {
+    // show the slider
+    plugin.show_response_slider(display_element, trial);
+    
+    var startTime = (new Date()).getTime();
+    
+    //show the prompt
+    if (trial.prompt !== "") {
           display_element.append(trial.prompt);
         }  
-        
-        if(trial.timeout > 0){
-      	  trial.setTimeoutHandlers.push(setTimeout(function(){
-          	  endTrial({rt:-1});
-            }, trial.timeout));
-        }
-        
-        $("#next").click(function(){
-      	  var endTime = (new Date()).getTime();
-            var response_time = endTime - startTime;
-      	  endTrial({
-      		  rt: response_time,
-      		  sim_score: $("#slider").slider("value")
-      	  });
-        });
-     }
-
-    // store response
-    var response = {
-      rt: -1,
-      key: -1
-    };
-
-    // function to end trial when it is time
-    var end_trial = function() {
-
-      // kill any remaining setTimeout handlers
-      for (var i = 0; i < setTimeoutHandlers.length; i++) {
-        clearTimeout(setTimeoutHandlers[i]);
-      }
-
-      // kill keyboard listeners
-      if (typeof keyboardListener !== 'undefined') {
-        jsPsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
-      }
-
-      // gather the data to store for the trial
-      var trial_data = {
-        "rt": response.rt,
-        "stimulus": trial.stimulus,
-        "key_press": response.key
-      };
-
-      //jsPsych.data.write(trial_data);
-
-      // clear the display
-      display_element.html('');
-
-      // move on to the next trial
-      jsPsych.finishTrial(trial_data);
-    };
-
-    // function to handle responses by the subject
-    var after_response = function(info) {
-
-      // after a valid response, the stimulus will have the CSS class 'responded'
-      // which can be used to provide visual feedback that a response was recorded
-      $("#jspsych-single-stim-stimulus").addClass('responded');
-
-      // only record the first response
-      if (response.key == -1) {
-        response = info;
-      }
-
-      if (trial.response_ends_trial) {
-        end_trial();
-      }
-    };
-
-    // start the response listener
-    if (JSON.stringify(trial.choices) != JSON.stringify(["none"])) {
-      var keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
-        callback_function: after_response,
-        valid_responses: trial.choices,
-        rt_method: 'date',
-        persist: false,
-        allow_held_key: false
-      });
+    //if there is a timeout, schedule the trial end    
+    if(trial.timeout > 0){
+    	trial.setTimeoutHandlers.push(setTimeout(function(){
+    		plugin.endTrial({rt:-1}, trial, display_element);
+        }, trial.timeout));
     }
-
-    // hide image if timing is set
-    if (trial.timing_stim > 0) {
-      var t1 = setTimeout(function() {
-        $('#jspsych-single-stim-stimulus').css('visibility', 'hidden');
-      }, trial.timing_stim);
-      setTimeoutHandlers.push(t1);
-    }
-
-    // end trial if time limit is set
-    if (trial.timing_response > 0) {
-      var t2 = setTimeout(function() {
-        end_trial();
-      }, trial.timing_response);
-      setTimeoutHandlers.push(t2);
-    }
-
+    // wait for a response    
+    $("#next").click(function(){
+  	  var endTime = (new Date()).getTime();
+        var response_time = endTime - startTime;
+  	  plugin.endTrial({
+  		  rt: response_time,
+  		  rating: $("#slider").slider("value")
+  	 }, trial, display_element)
+    });
   };
-
+  
   return plugin;
 })();
